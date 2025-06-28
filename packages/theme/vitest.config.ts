a27fa51ceef34afbec661172ae50501590360d16
Slava Dodonov <slavadodonov19@gmail.com>
2025-06-28 03:05:08 +0300

    feat: 🎸 fixed tests

--- packages/theme/vitest.config.ts
+++ packages/theme/vitest.config.ts
@@ -3,6 +3,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths({ root: '../../' })
+    tsconfigPaths({ root: '../../' }),
   ],
 })
